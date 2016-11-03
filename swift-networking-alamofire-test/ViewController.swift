98df319569024016fb11e1dd9b1684fe415b4d30
ZhaonanLi <zhaonanproject@gmail.com>
2016-11-02 22:41:48 -0700

    Initial Commit

---
+++ swift-networking-alamofire-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-networking-alamofire-test
+//
+//  Created by Zhaonan Li on 11/2/16.
+//  Copyright © 2016 Zhaonan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
